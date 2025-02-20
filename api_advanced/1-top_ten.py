381930d17d7c19aa7dba0b1be5d7ccb268a430c8
Emma001code <e.ngwoke@alustudent.com>
2025-02-20 11:49:25 -0800

    check

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,32 @@
-#!/usr/bin/python3
-""" top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    If the subreddit is invalid or doesn't exist, prints None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "python:subreddit.top_ten:v1.0 (by /u/yourusername)"}
+
+    # Make the request
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+
+        if posts:
+            for i, post in enumerate(posts[:10]):
+                print(post["data"]["title"])
+        else:
+            print(None)
+    else:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+# Example usage
+top_ten("python")
+
