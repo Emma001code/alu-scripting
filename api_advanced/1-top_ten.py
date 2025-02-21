ed6d37a840316eec6dbe1d14339c304fb53bb8a0
Emma001code <e.ngwoke@alustudent.com>
2025-02-21 02:29:54 -0800

    update file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,18 +11,15 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "MyBot/0.0.1"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        # If the subreddit is invalid or a redirect occurs, the status
-        # won't be 200.
         if response.status_code != 200:
             print(None)
             return
-
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
+        data = response.json().get("data", {})
+        posts = data.get("children", [])
+        for post in posts[:10]:
             title = post.get("data", {}).get("title")
             if title:
                 print(title)
